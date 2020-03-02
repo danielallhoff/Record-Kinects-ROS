eafc92380615b0e1bbbeb4bd53a009dd3ceb34cb
Daniel <danielallhoff@hotmail.com>
2020-03-02 11:56:30 +0000

    Prepared

---
+++ sync-project/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /ros_sync/Synthetic-Pipeline/sync-project/src
+# Build directory: /ros_sync/Synthetic-Pipeline/sync-project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(gatherer)
---
+++ sync-project/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/ros_sync/Synthetic-Pipeline/sync-project/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/ros_sync/Synthetic-Pipeline/sync-project/devel/lib:$LD_LIBRARY_PATH"
+export PWD="/ros_sync/Synthetic-Pipeline/sync-project/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/ros_sync/Synthetic-Pipeline/sync-project/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/ros_sync/Synthetic-Pipeline/sync-project/src:$ROS_PACKAGE_PATH"
